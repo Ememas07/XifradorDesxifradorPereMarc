01fdbdeb4fd229271642cd00fc3e373432e75d65
Marc <marcmashernandez@gmail.com>
2025-02-13 08:58:57 +0100

    Xifrador

--- Xifrador Desxifrador/src/Xifrador.java
+++ Xifrador Desxifrador/src/Xifrador.java
@@ -1,6 +1,31 @@
+import javax.swing.JOptionPane;
+
 public class Xifrador {
     public static void main(String[] args) {
-        System.out.println("hola 3");
-        
+        String frase = JOptionPane.showInputDialog("Introdueixi frase"); // Demanam a l'usuari que introdueixi la frase
+        String frasexifrada = encriptador(frase); // Passam el xifrador per la frase
+        System.out.println(frasexifrada); // Posam la frase xifrada i la donam per consola
+    }
+
+    public static String encriptador(String frase) { // Encriptam la frase
+        if (frase == null || frase.isEmpty()) { // Si esta buida no feim res
+            return "";
+        }
+        int ValorXifratge = (int) ((Math.random() + 1) * 100);
+        System.out.println("Valor Xifratge =" + ValorXifratge);
+        StringBuilder frasecanviada = new StringBuilder(); // StringBuilder ens crearà la frase encriptada
+        for (int index = 0; index < frase.length(); index++) { // Iniciam index com a 0, i aquest bucle es farà fins que
+                                                               // haguem xifrat tots els caràcters
+            int caracterxifrat = 0;
+            if (index < 1) {
+                char caracter = (char) ValorXifratge;
+                caracterxifrat = caracter * ValorXifratge;
+                frasecanviada.append(" ").append(caracterxifrat);
+            }
+            char caracter = frase.charAt(index);
+            caracterxifrat = caracter * ValorXifratge;
+            frasecanviada.append(" ").append(caracterxifrat);
+        }
+        return frasecanviada.toString().trim();
     }
 }
