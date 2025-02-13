de44f906a71e4be880d9758c01a16f95cd21eadf
Marc <marcmashernandez@gmail.com>
2025-02-13 08:24:09 +0100

    super algoritme

--- Xifrador Desxifrador/src/Xifrador.java
+++ Xifrador Desxifrador/src/Xifrador.java
@@ -1,5 +1,6 @@
 public class Xifrador {
     public static void main(String[] args) {
         System.out.println("hola 3");
+        
     }
 }
--- Xifrador Desxifrador/src/desxifrador.java
+++ Xifrador Desxifrador/src/desxifrador.java
@@ -1,4 +1,4 @@
-public class desxifrador {
+public class Desxifrador {
     public static void main(String[] args) {
         System.out.println("hola 4");
     }
